f0de90c852d61bdf67a1a2994d038fecada38321
Shivani Baskar <58472391+shivanibaskar@users.noreply.github.com>
2022-04-17 17:53:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import altair as alt
 import streamlit as st
 st.set_page_config(layout="wide")
-st.markdown("## Analysis Of Popularity Of Visualizations In [r/DataisBeautiful](https://www.reddit.com/r/dataisbeautiful/)  ")
+st.markdown("## Analysis of Popularity of Visualizations in [r/DataisBeautiful](https://www.reddit.com/r/dataisbeautiful/)  ")
 row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 with row3_1:
     st.markdown("##### As an anonymous social network, Reddit has a ton of interesting data to offer. Subreddits like r/dataisbeautiful and r/visualization are some \
@@ -13,7 +13,8 @@ of the best places to look for creative visualization ideas. But rarely does a v
 become popular or trending.")
     st.markdown("##### Oftentimes, complex visualizations that display a lot of information receive very few upvotes. For instance, aesthetics are not the only factor for a \
 visualization to gain upvotes as beautiful in the r/dataisbeautiful subreddit")
-    st.markdown("##### What factors cause an image to trend in the subreddit?")
+    st.markdown("##### What factors cause a visualization to trend in this subreddit? This would be the same answering what makes the Reddit feed algorithm tag a post as 'rising'?")
+    st.markdown("##### To answer this, we collected data in regular intervals over one week to obtain 100 trending posts and 70 non trending posts.")
     st.markdown("##### Let us consider the following metrics")
     st.markdown("##### - Number of Upvotes")
     st.markdown("##### - Number of Downvotes")
@@ -76,10 +77,12 @@ with st.container():
             labelFontSize=20,
             titleFontSize=20)
         trending_or_not_chart
+    st.markdown("#####   Trending posts gain a lot of attention and on an average, have more upvotes, downvotes and awards than non-trending posts")
+
   
 #Container 1
 with st.container():
-    st.subheader("How do the metrics correlate with one another?")
+    st.subheader("How do these metrics correlate with one another?")
     st.markdown("####   \n")
     col_spacer1,col1,spacer, col2,col2_spacer = st.columns((.1, 7,.1,3, .1))
     with col1:
@@ -123,10 +126,8 @@ with st.container():
     with col2:
         st.markdown("####   \n")
         st.markdown("####   \n")
-        st.markdown("##### This is point 1")
-        st.markdown("##### This is point 2")
-        st.markdown("##### This is point 3")
-
+        st.markdown("##### Author Karma doesn't affect the metrics.")
+        st.markdown("##### Upvotes, downvotes and total awards have significant positive correlation with each other, supporting the findings in the above graphs.")
 
 
         
@@ -149,6 +150,7 @@ with st.container():
         elif (atrribute_selectbox == atrribute_options[2]) :
             attribute = 'total_awards'
         st.markdown("#### Across different visualization types")
+        st.markdown("###### Click on a data point for preview of visualization and its details")
 
 with st.container():
     col_spacer1,col1, col2,col3,col3_spacer = st.columns((.1, 5,5,5, .1))
@@ -193,17 +195,29 @@ with st.container():
         mosaic3
 
 
+# Getting awards for tooltip
+list_of_awards = []
+for i in range(len(df)):
+    awards = df.iloc[i].awards[1:-2].split('), ')
+    award_list = []
+    for x in awards:
+        award_list.append(x[2:-4])
+    list_of_awards.append(','.join(award_list))
+df['list_of_awards'] = list_of_awards
+df.list_of_awards  = df.list_of_awards.apply(lambda x: x if len(x)>0 else 'None')
+
 with st.container():
     col_spacer1,col1,col2, spacer2 = st.columns((.1, 7,3, .1))
     with col1:
         st.markdown("#### Across different times of day")
+        st.markdown("###### Click on a data point for preview of visualization and its details")
         # Scatterplot 1 - By Hour of Creation
         scatterplot1 = alt.Chart(df).mark_circle().\
         encode(
-            alt.X('time_created:Q',title='Hour of Day',axis=alt.Axis(labelAngle=0),scale = alt.Scale(domain = [min(df.time_created),max(df.time_created)])),
+            alt.X('time_created:Q',title='Hour of Day (UTC)',axis=alt.Axis(labelAngle=0),scale = alt.Scale(domain = [min(df.time_created),max(df.time_created)])),
             alt.Y(attribute+':Q',title='Number of '+attribute.title()),
             alt.Color('is_trending:N',title=' ',legend=alt.Legend(orient='top')),
-            tooltip=['image','flairs','is_dynamic','is_interactive']
+            tooltip=['image',attribute,'flairs','is_dynamic','is_interactive','list_of_awards']
             ). \
         properties(width=700,height=500).configure_axis(
             labelFontSize=20,
@@ -224,16 +238,17 @@ with st.container():
         st.markdown("####   \n")
         st.markdown("####   \n")
         st.markdown("####   \n")
-        st.markdown("##### This is point 1")
-        st.markdown("##### This is point 2")
-        st.markdown("##### This is point 3")
-
-
+        st.markdown("####   \n")
+        st.markdown("##### Posts get better traction when posted between 6 AM and 4 PM UTC.")
+        st.markdown("##### Interesting find:")
+        st.markdown("##### Even some trending posts are found to have very low values of the considered popularity metric, showing that Reddit considers more information in deciding to tag a post as 'rising'.")
+        st.markdown("##### An explanation would be the relative time taken to get each vote/award from the time of posting.")
 
 with st.container():
     col_spacer1,col1,col2, spacer2 = st.columns((.1, 7,3, .1))
     with col1:
         st.markdown("#### For different number of colors in images")
+        st.markdown("###### Click on a data point for preview of its major colors (leaving out the most dominant/background color)")
         # Manipulations for second scatter plot
         img_df = pd.read_csv('color_data.csv')
         df['colors'] = img_df['number_of_colors']
@@ -246,7 +261,7 @@ with st.container():
             alt.X('colors:Q',title='Number of Colors',axis=alt.Axis(labelAngle=0),scale = alt.Scale(domain = [min(df.time_created),max(df.time_created)])),
             alt.Y(attribute+':Q',title='Number of '+attribute.title()),
             alt.Color('is_trending:N',title=' ',legend=alt.Legend(orient='top')),
-            tooltip=['image','flairs','is_dynamic','is_interactive']
+            tooltip=['image',attribute,'flairs','is_dynamic','is_interactive','list_of_awards']
             ). \
         properties(width=700,height=500).configure_axis(
             labelFontSize=20,
@@ -267,23 +282,24 @@ with st.container():
         st.markdown("####   \n")
         st.markdown("####   \n")
         st.markdown("####   \n")
-        st.markdown("##### This is point 1")
-        st.markdown("##### This is point 2")
-        st.markdown("##### This is point 3")
+        st.markdown("####   \n")
+        st.markdown("##### While not every trending post has a lot of colors, it takes at least 4 colors for a trending post to get an award or to cross 1000 upvotes.")
+        st.markdown("##### As we explore the data points looking at their tooltip information, it can be noticed that among the posts with more number of colors, the posts that trend tend to utilize bright and contrasting colors compared to the relatively dull ones in the non trending posts.")
 
 
 with st.container():
-    data['author_has_reddit_premium'] = data.author_has_reddit_premium.apply(lambda x: 'True' if x==1 else 'False')
+    data['author_has_reddit_premium'] = data.author_has_reddit_premium.apply(lambda x: 'Premium User' if x==1 else 'Not a Premium User')
     col_spacer1,col1,col2, spacer2 = st.columns((.1, 7,3, .1))
     with col1:
-        st.markdown("#### With Authors karma")
+        st.markdown("#### What about Reddit Premium users and seasoned users with high 'karma'?")
         plot5=alt.Chart(data).mark_circle(size=60).encode(
-        x=alt.X('author_karma:Q',axis=alt.Axis(title="Author Karma",format=".2s"),scale=alt.Scale(domain=[0,400000])),
+        x=alt.X('author_karma:Q',axis=alt.Axis(title="Author Karma"),scale=alt.Scale(domain=[0,400000])),
         y=alt.Y(attribute+':Q',title='Number of '+attribute.title()),
-        color=alt.Color('author_has_reddit_premium',legend=alt.Legend(title="Premium User",orient="top")),
+        color=alt.Color('author_has_reddit_premium',legend=alt.Legend(title="Author Account Type",orient="top")),
         tooltip = [
+             alt.Tooltip(field = attribute, type = "quantitative",title = 'Number of '+attribute.title()),
             alt.Tooltip(field = 'author_karma', type = "quantitative",title = "Author Karma"),
-            alt.Tooltip(field = 'total_awards', type = "quantitative",title = "Total Awards")]).properties(width=700,height=500)\
+           ]).properties(width=700,height=500)\
             .configure_axis(
             labelFontSize=20,
             titleFontSize=20).configure_legend(titleColor='black', titleFontSize=18,labelFontSize=18).interactive()
@@ -295,10 +311,68 @@ with st.container():
         st.markdown("####   \n")
         st.markdown("####   \n")
         st.markdown("####   \n")
-        st.markdown("##### This is point 1")
-        st.markdown("##### This is point 2")
-        st.markdown("##### This is point 3")
+        df['author_has_reddit_premium'] = df.author_has_reddit_premium.apply(lambda x: 'Premium User' if x==1 else 'Not a Premium User')
+        mosaic4 = alt.Chart(df).mark_bar(width=30).\
+        encode(
+            alt.X('author_has_reddit_premium:N',title='Author Acount Type',axis=alt.Axis(labelAngle=0)),
+            alt.Y('mean('+attribute+'):Q',title='Avg Number of '+attribute.title()),
+            alt.Color('is_trending:N',title=' ',legend=alt.Legend(orient='top')),
+            ). \
+        properties(width=350,height=350).configure_axis(
+            labelFontSize=20,
+            titleFontSize=20).configure_legend(titleColor='black', titleFontSize=18,labelFontSize=18)
+        mosaic4
+
+with st.container():
+    col_spacer1,col1,col2, spacer2 = st.columns((.3, 50,0.1, .1))
+    with col1:
+        st.markdown("##### While the authors with high karma do not have great posts, it has to be remembered that the correlation map showed no relation between metrics and author karma. The explanation lies in observing that while the users with lower levels of karma have great posts, a majority of the low karma users have mediocre posts lying on the lower end of the votes and awards counts.")
 
 
+with st.container():
+    col_spacer1,col1,col2, spacer2 = st.columns((.1, 7,3, .1))
+    with col1:
+        st.markdown("#### In summary, what do most trending posts have in common?")
+with st.container():
+    col_spacer1,col1,col2, spacer2 = st.columns((.3, 50,0.1, .1))
+    with col1:
+        st.markdown("##### 1. Trending posts are mostly dynamic, interactive and are original content.")
+        st.markdown("##### 2. They are posted usually during the times when the rest of the world is active on this subreddit - the first half of the day (before 12 PM EDT).")
+        st.markdown("##### 3. Trending posts have bright, constrasting and easily distinguishable colors.")
+        st.markdown("##### 4. While the posts of Premium users trend more, there are some non Premium users whose posts perform even better!")
 
 
+with st.container():
+    col_spacer1,col1,col2, spacer2 = st.columns((.1, 7,3, .1))
+    with col1:
+        st.markdown("###### Thanks for reading this article :)")
+        st.markdown("###### Github repository containing data and analysis [here](https://www.reddit.com/r/dataisbeautiful/)")
+
+
+# AUTHOR'S KARMA AFFECTING ATTRIBUTE
+    # with col2:
+    #     st.subheader("Does an author with a better Karma score more likely to get more awards?")
+    #     plot2=alt.Chart(data).mark_circle(size=60).encode(
+    #     x=alt.X('author_karma:Q',axis=alt.Axis(grid=False,title="Author Karma"),scale=alt.Scale(domain=[0,400000])),
+    #     y=alt.Y('total_awards:Q',axis=alt.Axis(grid=False,title="Total awards")),
+    #     color=alt.Color('author_has_reddit_premium',legend=alt.Legend(title="Premium User")),
+    #     tooltip = [
+    #         alt.Tooltip(field = 'author_karma', type = "quantitative",title = "Author Karma"),
+    #         alt.Tooltip(field = 'total_awards', type = "quantitative",title = "Total Awards"),
+    #         alt.Tooltip(field = 'author_has_reddit_premium', type = "quantitative",title = "Premium User")]).interactive()
+    #     st.write(plot2.properties(width=550,height=550))
+
+# ATTRIBUTE AND AUTHOR KARMA
+# filter_data = data[["urls",'author_karma',"total_awards","total_votes",'author_has_reddit_premium']]
+# data1 =filter_data.melt(['urls','author_karma','author_has_reddit_premium'], var_name='attribute', value_name='count')
+# attribute_selected = st.radio("Select Emoji",('total_awards', 'total_votes'))
+# plot3=alt.Chart(data1).mark_circle(size=60).encode(
+#     x=alt.X('author_karma:Q',axis=alt.Axis(grid=False,title="Author Karma"),scale=alt.Scale(domain=[0,400000])),
+#     y=alt.Y('count:Q',axis=alt.Axis(grid=False,title="Attribute")),
+#     color=alt.Color('author_has_reddit_premium',legend=alt.Legend(title="Premium User")),
+#     tooltip = [
+#         alt.Tooltip(field = 'author_karma', type = "quantitative",title = "Author Karma"),
+#         alt.Tooltip(field = 'total_awards', type = "quantitative",title = "Total Awards"),
+#         alt.Tooltip(field = 'author_has_reddit_premium', type = "quantitative",title = "Premium User")]
+# ).transform_filter(attribute_selected == alt.datum.attribute).interactive()
+# st.write(plot3)    
